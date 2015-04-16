ed49b04ac5878612640f0fbc421e7e396ad5578a
cakl <sebastian.bock@bluewin.ch>
2015-04-16 04:54:00 +0200

    added invalidation of displaylink

--- Waver/Source/Waver.m
+++ Waver/Source/Waver.m
@@ -72,27 +72,32 @@
 
 -(void)setWaverLevelCallback:(void (^)())waverLevelCallback
 {
-    _waverLevelCallback = waverLevelCallback;
-    
-    CADisplayLink *displaylink = [CADisplayLink displayLinkWithTarget:_waverLevelCallback selector:@selector(invoke)];
-    [displaylink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
-    
-    for(int i=0; i < self.numberOfWaves; i++)
-    {
-        CAShapeLayer *waveline = [CAShapeLayer layer];
-        waveline.lineCap       = kCALineCapButt;
-        waveline.lineJoin      = kCALineJoinRound;
-        waveline.strokeColor   = [[UIColor clearColor] CGColor];
-        waveline.fillColor     = [[UIColor clearColor] CGColor];
-        [waveline setLineWidth:(i==0 ? self.mainWaveWidth : self.decorativeWavesWidth)];
-        CGFloat progress = 1.0f - (CGFloat)i / self.numberOfWaves;
-        CGFloat multiplier = MIN(1.0, (progress / 3.0f * 2.0f) + (1.0f / 3.0f));
-		UIColor *color = [self.waveColor colorWithAlphaComponent:(i == 0 ? 1.0 : 1.0 * multiplier * 0.4)];
-		waveline.strokeColor = color.CGColor;
-        [self.layer addSublayer:waveline];
-        [self.waves addObject:waveline];
+    if(!waverLevelCallback){
+        _waverLevelCallback = nil;
+        [self.displaylink invalidate];
+    } else {
+        _waverLevelCallback = waverLevelCallback;
+        if(self.displaylink) [self.displaylink invalidate];
+        
+        self.displaylink = [CADisplayLink displayLinkWithTarget:_waverLevelCallback selector:@selector(invoke)];
+        [self.displaylink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+        
+        for(int i=0; i < self.numberOfWaves; i++)
+        {
+            CAShapeLayer *waveline = [CAShapeLayer layer];
+            waveline.lineCap       = kCALineCapButt;
+            waveline.lineJoin      = kCALineJoinRound;
+            waveline.strokeColor   = [[UIColor clearColor] CGColor];
+            waveline.fillColor     = [[UIColor clearColor] CGColor];
+            [waveline setLineWidth:(i==0 ? self.mainWaveWidth : self.decorativeWavesWidth)];
+            CGFloat progress = 1.0f - (CGFloat)i / self.numberOfWaves;
+            CGFloat multiplier = MIN(1.0, (progress / 3.0f * 2.0f) + (1.0f / 3.0f));
+            UIColor *color = [self.waveColor colorWithAlphaComponent:(i == 0 ? 1.0 : 1.0 * multiplier * 0.4)];
+            waveline.strokeColor = color.CGColor;
+            [self.layer addSublayer:waveline];
+            [self.waves addObject:waveline];
+        }
     }
-    
 }
 
 - (void)setLevel:(CGFloat)level
